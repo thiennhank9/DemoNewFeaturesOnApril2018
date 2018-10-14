f26d28206852eb2d9dfaed6aa24b3e4fe2e3475e
nhan <thiennhank9@gmail.com>
2018-10-14 16:52:30 +0700

    #Upload project

---
+++ PicturesAndMusicLibraryTreeView/MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PicturesAndMusicLibraryTreeView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitializeTreeView();
+        }
+
+        private void InitializeTreeView()
+        {
+            // A TreeView can have more than 1 root node. The Pictures library
+            // and the Music library will each be a root node in the tree.
+            // Get Pictures library.
+            StorageFolder picturesFolder = KnownFolders.PicturesLibrary;
+            TreeViewNode pictureNode = new TreeViewNode();
+            pictureNode.Content = picturesFolder;
+            pictureNode.IsExpanded = true;
+            pictureNode.HasUnrealizedChildren = true;
+            sampleTreeView.RootNodes.Add(pictureNode);
+            FillTreeNode(pictureNode);
+
+            // Get Music library.
+            StorageFolder musicFolder = KnownFolders.MusicLibrary;
+            TreeViewNode musicNode = new TreeViewNode();
+            musicNode.Content = musicFolder;
+            musicNode.IsExpanded = true;
+            musicNode.HasUnrealizedChildren = true;
+            sampleTreeView.RootNodes.Add(musicNode);
+            FillTreeNode(musicNode);
+        }
+
+        private async void FillTreeNode(TreeViewNode node)
+        {
+            // Get the contents of the folder represented by the current tree node.
+            // Add each item as a new child node of the node that's being expanded.
+
+            // Only process the node if it's a folder and has unrealized children.
+            StorageFolder folder = null;
+            if (node.Content is StorageFolder && node.HasUnrealizedChildren == true)
+            {
+                folder = node.Content as StorageFolder;
+            }
+            else
+            {
+                // The node isn't a folder, or it's already been filled.
+                return;
+            }
+
+            IReadOnlyList<IStorageItem> itemsList = await folder.GetItemsAsync();
+
+            if (itemsList.Count == 0)
+            {
+                // The item is a folder, but it's empty. Leave HasUnrealizedChildren = true so
+                // that the chevron appears, but don't try to process children that aren't there.
+                return;
+            }
+
+            foreach (var item in itemsList)
+            {
+                var newNode = new TreeViewNode();
+                newNode.Content = item;
+
+                if (item is StorageFolder)
+                {
+                    // If the item is a folder, set HasUnrealizedChildren to true. 
+                    // This makes the collapsed chevron show up.
+                    newNode.HasUnrealizedChildren = true;
+                }
+                else
+                {
+                    // Item is StorageFile. No processing needed for this scenario.
+                }
+                node.Children.Add(newNode);
+            }
+            // Children were just added to this node, so set HasUnrealizedChildren to false.
+            node.HasUnrealizedChildren = false;
+        }
+
+        private void SampleTreeView_Expanding(TreeView sender, TreeViewExpandingEventArgs args)
+        {
+            if (args.Node.HasUnrealizedChildren)
+            {
+                FillTreeNode(args.Node);
+            }
+        }
+
+        private void SampleTreeView_Collapsed(TreeView sender, TreeViewCollapsedEventArgs args)
+        {
+            args.Node.Children.Clear();
+            args.Node.HasUnrealizedChildren = true;
+        }
+
+        private void SampleTreeView_ItemInvoked(TreeView sender, TreeViewItemInvokedEventArgs args)
+        {
+            var node = args.InvokedItem as TreeViewNode;
+            if (node.Content is IStorageItem item)
+            {
+                FileNameTextBlock.Text = item.Name;
+                FilePathTextBlock.Text = item.Path;
+                TreeDepthTextBlock.Text = node.Depth.ToString();
+
+                if (node.Content is StorageFolder)
+                {
+                    node.IsExpanded = !node.IsExpanded;
+                }
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            sampleTreeView.RootNodes.Clear();
+            InitializeTreeView();
+        }
+    }
+}
---
+++ TreeViewWithSelectionEnabled/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TreeViewWithSelectionEnabled
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FlavorList.Text = string.Empty;
+            ToppingList.Text = string.Empty;
+
+            foreach (TreeViewNode node in DessertTree.SelectedNodes)
+            {
+                if (node.Parent.Content?.ToString() == "Flavors")
+                {
+                    FlavorList.Text += node.Content + "; ";
+                }
+                else if (node.HasChildren == false)
+                {
+                    ToppingList.Text += node.Content + "; ";
+                }
+            }
+        }
+
+        private void SelectAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DessertTree.SelectionMode == TreeViewSelectionMode.Multiple)
+            {
+                DessertTree.SelectAll();
+            }
+        }
+    }
+}
